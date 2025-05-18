4384ebd34c7e0fd08efa609fe022afe1161a1676
Akachukwu Aneke <akalodave@gmail.com>
2025-05-18 16:04:46 +0100

    fixed bug

--- app/Http/Requests/UpdateLive.php
+++ app/Http/Requests/UpdateLive.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use App\Http\Requests\BaseRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateLive extends BaseRequest
 {
